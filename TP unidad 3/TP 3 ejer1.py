28b108e6e0c792434393d01f215b891d73243e7c
Ciro-Cattaneo <cirocattaneo14@gmail.com>
2025-09-15 20:43:38 -0300

    Carpeta TP3
    Carpeta tp3 con los dos primeros puntos

---
+++ TP unidad 3/TP 3 ejer1.py
@@ -0,0 +1,7 @@
+edad= int(input("Ingrese su edad  "))
+#El programa pide que debe ser MAYOR(no mayor o igual) a 18 años para pasar el condicional 
+if edad> 18 :
+    print("Es mayor de edad")
+#No pedia un else pero lo hice igual
+else:
+    print("Es menor de edad")
