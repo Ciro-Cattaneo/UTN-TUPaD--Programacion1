892289f0a6f82be2ce4a0109faa791a27e146109
Ciro-Cattaneo <cirocattaneo14@gmail.com>
2025-09-19 15:20:19 -0300

    TP4 ejer 6 y 7
    ejercicios 6 y 7 resueltos

---
+++ TP unidad 4/TP 4 ejer6.py
@@ -0,0 +1,5 @@
+num=100
+while num>=0 :
+    if num%2 ==0:
+        print(num)
+    num-=1
---
+++ TP unidad 4/TP 4 ejer7.py
@@ -0,0 +1,5 @@
+num=int(input("Ingrese un numero  "))
+suma=0
+for i in range(num):
+    suma= suma+i
+print("La suma de todos los numeros comprendidos entre 0 y el numero ingresado es ",suma)
