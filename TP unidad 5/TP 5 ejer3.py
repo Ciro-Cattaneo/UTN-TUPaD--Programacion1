ead188f90f634de072de2401c47606b49aab94e8
Ciro-Cattaneo <cirocattaneo14@gmail.com>
2025-09-22 22:11:50 -0300

    TP 5 ejer3 y 4
    ejercicios 3 y 4

---
+++ TP unidad 5/TP 5 ejer3.py
@@ -0,0 +1,10 @@
+lista=[]
+print("Agreguemos palabras a la lista ")
+pal1= input("Agrega la primera palabra ")
+lista.append(pal1)
+pal2= input("Agrega la segunda palabra ")
+lista.append(pal2)
+pal3= input("Agrega la tercera palabra ")
+lista.append(pal3)
+print(lista)
+#SE PODRÍA HACER MAS CORTO USANDO UN FOR 
